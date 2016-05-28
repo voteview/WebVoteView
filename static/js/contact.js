3ba670d61ee9bc23d8c9a261e64ba897a19422fb
aaronrudkin <junk_signups@rudkin.ca>
2016-05-27 21:55:26 -0700

    Contact form implemented.

---
+++ static/js/contact.js
@@ -0,0 +1,26 @@
+function submitForm()
+{
+	$.ajax({
+		type: "POST",
+		url: "/api/contact",
+		data: $('#contact-form').serialize(),
+		beforeSend:function(){return;},
+		success: function(res, status, xhr)
+		{
+			if(res["success"])
+			{
+				var span = $("<span></span>").html("Thank you for contactig us. We will get back to you as soon as possible.");
+				$("#result > span").remove();
+				span.appendTo($("#result"));
+				$("#result").fadeIn();
+				$("#contact-form").slideUp();
+			}
+			else
+			{
+				var span = $("<span></span>").html(res["error"]).css("color","red");
+				$("#result > span").remove();
+				span.appendTo($("#result"));
+				$("#result").fadeIn();
+			}
+		}});
+}
