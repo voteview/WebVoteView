f4b7eec6410fe3a8fd64b50833dfb749b1eaea93
aaronrudkin <junk_signups@rudkin.ca>
2016-06-11 14:24:24 -0700

    More work on Explore Senators/Representatives pages. Preliminaries on adding a Nominate plot.

--- static/js/congress.js
+++ static/js/congress.js
@@ -1,5 +1,6 @@
 var resultCache;
 var sortBy = "elected";
+var nominateScatterChart = dc.scatterPlot("#scatter-chart");
 
 // From stackoverflow response, who borrowed it from Shopify--simple ordinal suffix.
 function getGetOrdinal(n) {
@@ -16,13 +17,76 @@ $(document).ready(function()
 		success: function(data, status, xhr)
 		{
 			$('#loading-container').slideUp();
-			resultCache = data["results"];
+			resultCache = data;
 			writeBioTable();
+			nomPlot();
 		}
 	});
 
 	$("#congSelector").change(reloadBios);
 });
+
+function nomPlot()
+{
+	console.log('ok');
+	var ndx = crossfilter(resultCache["results"]);
+	var all = ndx.groupAll();
+	var xDimension = ndx.dimension(
+		function(d) 
+		{
+			var x = d.nominate.oneDimNominate;
+			var y = d.nominate.twoDimNominate;
+			var R = Math.sqrt(x*x + y*y);
+			if (R>1.2)
+			{
+				x = x*1.2/R;
+				y = y*1.2/R;
+			}
+			return [x,y];
+		}
+	);
+	var xGroup = xDimension.group().reduce(
+		function(p, d)
+		{
+			p.members.push(d);
+			return p;
+		},
+	
+		function(p, d)
+		{
+			var index = p.members.indexOf(d);
+			if(index > -1) { p.members.splice(index, 1); }
+			return p;
+		},
+	
+		function() { return {members: []} ; }
+	);
+
+	nominateScatterChart
+	.width(600)
+	.height(600)
+	.margins({top:25,right:25,bottom:75,left:75})
+	.dimension(xDimension)
+	.mouseZoomable(false)
+	.group(xGroup)
+	.symbolSize(7)
+	.colorCalculator(function(d) {
+		var color = "#CCC";
+		try {
+			if(d.value.members.length > 0){
+				color = blendColors(d.value.members);
+			}
+		}catch(e){
+		}
+		return color;
+	})
+	.highlightedSize(7)
+	.x(d3.scale.linear().domain([-1.2,1.2]))
+	.y(d3.scale.linear().domain([-1.2,1.2]));
+
+	dc.renderAll();
+	decorateNominate(nominateScatterChart, resultCache);
+}
 
 function resort(sortB)
 {
@@ -40,19 +104,22 @@ function reloadBios()
 		success: function(data, status, xhr)
 		{
 			$("#memberList").fadeIn();
-			resultCache = data["results"];
+			resultCache = data;
 			writeBioTable();
+			nomPlot();
 		}
 	});
 }
 
 function writeBioTable()
 {
-	if(sortBy=="name") { resultCache.sort(function(a,b) { return a.bioName > b.bioName ? 1 : -1; }); }
-	else if(sortBy=="party") { resultCache.sort(function(a,b) { return (a.partyname==b.partyname)?(a.bioName>b.bioName?1:-1):(a.partyname>b.partyname?1:-1); }); }
-	else if(sortBy=="elected") { resultCache.sort(function(a,b) { return (a.minElected==b.minElected)?(a.bioName>b.bioName?1:-1):(a.minElected>b.minElected?1:-1); }); }
+	rC = resultCache["results"];
+	if(sortBy=="name") { rC.sort(function(a,b) { return a.bioName > b.bioName ? 1 : -1; }); }
+	else if(sortBy=="party") { rC.sort(function(a,b) { return (a.partyname==b.partyname)?(a.bioName>b.bioName?1:-1):(a.partyname>b.partyname?1:-1); }); }
+	else if(sortBy=="state") { rC.sort(function(a,b) { return(a.stateName==b.stateName)?(a.bioName>b.bioName?1:-1):(a.stateName>b.stateName?1:-1); }); }
+	else if(sortBy=="elected") { rC.sort(function(a,b) { return (a.minElected==b.minElected)?(a.bioName>b.bioName?1:-1):(a.minElected>b.minElected?1:-1); }); }
 	$("#memberList").html("");
-	$.each(resultCache,function(k, v)
+	$.each(rC,function(k, v)
 	{
 		constructPlot(v);
 	});
--- views/congress.tpl
+++ views/congress.tpl
@@ -22,9 +22,26 @@
 				(Sort by
 				<a href="#" onclick="javascript:resort('name');return false;">Name</a>, 
 				<a href="#" onclick="javascript:resort('party');return false;">Party</a>, 
+				<a href="#" onclick="javascript:resort('state');return false;">State</a>, 
 				<a href="#" onclick="javascript:resort('elected');return false;">Seniority</a>)
 			</div>
 		</div>
+
+		<!-- Nominate graph -->
+		<!--<h4>DW-Nominate Plot
+			<a href="#" onclick="javascript:saveSvgAsPng($('#scatter-chart > svg')[0],'plot_{{memberLabel}}.png', {backgroundColor: 'white'});return false;">
+				<img src="/static/img/save.png" style="margin-left:5px;width:22px;vertical-align:middle;" data-toggle="tooltip" data-position="bottom" data-html="true" title="Save Plot as PNG">
+			</a>
+		</h4>
+
+		<div id="scatter-container" style="margin:0 auto 0 auto;">
+			<div id="scatter-bg">
+				<svg id="svg-bg"></svg> 
+			</div>
+			<div id="scatter-chart">
+			</div>
+		</div>-->
+
 		<div id="memberList" style="margin-bottom:40px;" class="clearfix">
 		</div>
 	</div>
@@ -34,5 +51,11 @@
 var chamber_param = "{{ chamber }}";
 var congressNum = 114;
 </script>
+<script type="text/javascript" src="{{ STATIC_URL }}js/libs/sprintf.min.js"></script>
+<script type="text/javascript" src="{{ STATIC_URL }}js/libs/d3.min.js"></script>
+<script type="text/javascript" src="{{ STATIC_URL }}js/libs/crossfilter.min.js"></script>
+<script type="text/javascript" src="{{ STATIC_URL }}js/libs/dc.min.js"></script>
+<script type="text/javascript" src="{{ STATIC_URL }}js/colorMap.js"></script>
+<script type="text/javascript" src="{{ STATIC_URL }}js/decorate.js"></script>
 <script type="text/javascript" src="{{ STATIC_URL }}js/congress.js"></script>
 
