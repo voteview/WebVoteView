eff572408eb525c9659e0cb4c87e3a37db75fad0
Luke Sonnet <luke.sonnet@gmail.com>
2017-02-04 08:58:56 -0800

    UPDATE After Adam synced descriptions

--- model/partyData.py
+++ model/partyData.py
@@ -2,8 +2,13 @@ from pymongo import MongoClient
 import json
 
 client = MongoClient()
-dbConf = json.load(open("./model/db.json","r"))
-db = client[dbConf["dbname"]]
+try:
+	dbConf = json.load(open("./model/db.json","r"))
+	db = client[dbConf["dbname"]]
+except:
+	dbConf = json.load(open("./db.json","r"))
+	db = client[dbConf["dbname"]]
+
 
 def getPartyData(code, api="Web_Name"):
 	try:
@@ -11,17 +16,17 @@ def getPartyData(code, api="Web_Name"):
 	except:
 		return {"error": "Invalid party code requested."}
 
-	r = db.voteview_parties.find_one({"id": code},{"partyname": 1, "count": 1, "fullName": 1, "pluralNoun": 1, "noun": 1, "briefName": 1, "partyDesc": 1, "_id": 0})
+	r = db.voteview_parties.find_one({"id": code},{"partyname": 1, "count": 1, "fullName": 1, "pluralNoun": 1, "noun": 1, "briefName": 1, "party_description": 1, "_id": 0})
 	if not r or r is None or not "partyname" in r:
 		return {"error": "Party code not found.", "partyname": None}
 	else:
 		retDict = {"partyname": r["partyname"], "fullName": r["fullName"], "pluralNoun": r["pluralNoun"], "noun": r["noun"]}
-		if "partyDesc" in r:
+		if "party_description" in r:
 			retDict["party_description"] = r["party_description"]
 		if "briefName" in r:
 			retDict["briefName"] = r["briefName"]
 		return retDict
 
 if __name__=="__main__":
-	print getPartyName(13)
-	print getPartyName("fart")
+	print getPartyData(22)
+	print getPartyData("fart")
