d51f990343a18031ef24ec58fb07765a65c8075c
aaronrudkin <junk_signups@rudkin.ca>
2021-01-22 09:58:06 -0800

    Fixes #399, off by one error in the conversion

--- static/js/voteFilterbar.js
+++ static/js/voteFilterbar.js
@@ -103,12 +103,12 @@ function pollFilters2()
 		var endXY = coordSets[1].split(",");
 		initXY[0] = parseFloat(initXY[0].substr(1)).toPrecision(2);
 		initXY[1] = parseFloat(initXY[1]).toPrecision(2);
-		endXY[0] = parseFloat(endXY[0].substr(0,endXY[0].length-1)).toPrecision(2);
+		endXY[0] = parseFloat(endXY[0].substr(0, endXY[0].length)).toPrecision(2);
 		endXY[1] = parseFloat(endXY[1]).toPrecision(2);
-		var resultText = "("+initXY[0]+", "+initXY[1]+") to ("+endXY[0]+", "+endXY[1]+")";
+		var resultText = "(" + initXY[0] + ", " + initXY[1] + ") to (" + endXY[0] + ", " + endXY[1] + ")";
 		$("#nominate-chart-controls > .filter").text(resultText);
 		$("#nominate-chart-controls").show();
-		baseFilters=baseFilters+1;
+		baseFilters = baseFilters + 1;
 	}
 	else
 	{
